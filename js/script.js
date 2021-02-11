d8a9a6aff8691f54df65d3768f9ab2f58b1f3c9b
Karolina Zioło <karolina.ziolo@gmail.com>
2021-02-11 15:08:14 +0100

    Add all js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
  function titleClickHandler(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
   /* tu powinna być console.log, który wyświetli zawartość argumentu event ale u mnie się nic nie wyświetla :( */
 
   
-/* usuń klasę „aktywną” ze wszystkich linków do artykułów - remove class 'active' from all article links */
+/* [DONE] usuń klasę „aktywną” ze wszystkich linków do artykułów - remove class 'active' from all article links */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -13,9 +15,13 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-/* dodaj klasę „aktywna” do klikniętego linku - add class 'active' to the clicked link */
+/* [DONE} dodaj klasę „aktywna” do klikniętego linku - add class 'active' to the clicked link */
 
-/* usuń klasę „aktywna” ze wszystkich artykułów - remove class 'active' from all articles */
+clickedElement.classList.add('active');
+console.log('clickedElement:', clickedElement);
+
+
+/* [DONE] usuń klasę „aktywna” ze wszystkich artykułów - remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('article');
 
@@ -24,10 +30,16 @@ for(let activeArticle of activeArticles){
 }
 
 /* pobierz atrybut „href” z klikniętego linku - get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute('href')
 
 /* znajdź poprawny artykuł za pomocą selektora (wartość atrybutu „href”) - find the correct article using the selector (value of 'href' attribute) */
 
+const targetArticle = document.querySelector(articleSelector);
+
 /* dodaj klasę „active” do poprawnego artykułu - add class 'active' to the correct article */
+
+targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
