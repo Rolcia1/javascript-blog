9e01a698496dab4d80c38a5e2c253c6c32d1f431
Karolina Zioło <karolina.ziolo@gmail.com>
2021-02-11 11:40:49 +0100

    Add remove class

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 'use strict';
 
-function titleClickHandler(event){
+ function titleClickHandler(event){
     console.log('Link was clicked!');
   /* tu powinna być console.log, który wyświetli zawartość argumentu event ale u mnie się nic nie wyświetla :( */
 
   
 /* usuń klasę „aktywną” ze wszystkich linków do artykułów - remove class 'active' from all article links */
 
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
 /* dodaj klasę „aktywna” do klikniętego linku - add class 'active' to the clicked link */
 
 /* usuń klasę „aktywna” ze wszystkich artykułów - remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('article');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
 /* pobierz atrybut „href” z klikniętego linku - get 'href' attribute from the clicked link */
 
