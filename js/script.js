75ee7fe01a8ca73d63c624755088b86ebfca737e
Karolina Zioło <karolina.ziolo@gmail.com>
2021-02-17 23:16:37 +0100

    Add broken tags

--- js/script.js
+++ js/script.js
@@ -46,7 +46,8 @@ function titleClickHandler(event){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
     
@@ -85,6 +86,52 @@ function generateTitleLinks(){
 
 }
 
-
 generateTitleLinks();
 
+
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+  for(let article of articles){
+    console.log(article);
+
+    /* find tags wrapper */
+    const titleList = article.querySelector(optTitleListSelector);
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags'),
+
+    /* split tags into array */
+      articleTagsArray = articleTags.split(' ');
+
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag' + tag+ '"><span>' + tag + '</span></a></li>';
+
+      /* add generated code to html variable */
+      html += linkHTML;
+    
+    /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+    titleList.innerHTML = html;
+    const links = article.querySelectorAll('.post-tags .list');
+
+
+
+  /* END LOOP: for every article: */
+  }
+  
+  
+
+}
+
+generateTags();
